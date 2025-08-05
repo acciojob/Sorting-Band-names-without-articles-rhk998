2f3846cd0202195f556d1c0600af2474e85fe31c
rhk998 <130037816+rhk998@users.noreply.github.com>
2025-08-05 13:08:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];   
+const sortedTours = touristSpots.sort((a, b) => {
+  const cleanA = a.replace(/^(a|an|the)\s/i, '');
+  const cleanB = b.replace(/^(a|an|the)\s/i, '');
+  return cleanA.localeCompare(cleanB);
+});
+
+const ul = document.createElement('ul');
+ul.id = 'band'; 
+
+sortedTours.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
+
+document.body.appendChild(ul);
